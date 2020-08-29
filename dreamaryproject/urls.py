b6e363c72eb5ab3e75b1af2a36355b5bc93535e9
h0han <hyhan@dgist.ac.kr>
2020-08-30 04:01:31 +0900

    Add base.html, inheritance

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -18,10 +18,9 @@ from django.urls import path
 from page import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home'),
-    path('introduce/', views.introduce, name = "introduce"),
-    path('profile/<int:designer_id>', views.detail, name = "detail"),
+    path('', include('page.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
