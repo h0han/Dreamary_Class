fe189178e470700c0191a874c66c66a5a771e50e
h0han <hyhan@dgist.ac.kr>
2020-09-06 17:39:01 +0900

    Revisde on Sep 6

--- page/urls.py
+++ page/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('introduce/', views.introduce, name = "introduce"),
-    path('profile/<int:designer_id>', views.detail, name = "detail"),
+    path('', views.home, name="home"),
+    path('profile/<int:designer_id>/', views.detail, name="detail"),
+    path('introduce/', views.introduce, name="introduce"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+    path('update/<int:designer_id>/', views.update, name="update"),
+    path('delete/<int:designer_id>/', views.delete, name="delete"),
 ]
